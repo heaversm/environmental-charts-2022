4292a0ee46ae01f2b53d14cc37d4ecc47638ab43
Mike Heavers <mheavers@gmail.com>
2022-12-30 15:37:09 -0800

    add stacked example

--- index.js
+++ index.js
@@ -6,35 +6,99 @@ new Chart(ctx, {
   type: "bar",
   data: {
     labels: [
-      "Donate effectively",
       "Have one fewer child",
-      "Live car-free",
-      "Switch to green heating",
-      "Avoid one transatlantic flight",
-      "Deep retrofit of house",
-      "Buy green electricity",
-      "Switch to electric car",
+      "Donate effectively",
+      "All Other Measures",
     ],
     datasets: [
       {
-        label: "tons CO2",
-        data: [100, 60, 2.4, 2, 1.6, 1.6, 1.4, 1.4],
+        label: "Have one less child",
+        data: [60, 0, 0],
         borderWidth: 1,
-        backgroundColor: [
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(201, 203, 207, 0.2)",
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-          "rgba(255, 205, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-        ],
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        stack: "Stack 0",
+      },
+      {
+        label: "Donate effectively",
+        data: [0, 100, 0],
+        borderWidth: 1,
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        stack: "Stack 1",
+      },
+      {
+        label: "Switch to Green Heating",
+        data: [0, 0, 2],
+        borderWidth: 1,
+        backgroundColor: "rgba(153, 102, 255, 0.2)",
+        stack: "Stack 2",
+      },
+      {
+        label: "Avoid one transatlantic flight",
+        data: [0, 0, 2],
+        borderWidth: 1,
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        stack: "Stack 2",
+      },
+      {
+        label: "Deep energy retrofit",
+        data: [0, 0, 1.6],
+        borderWidth: 1,
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        stack: "Stack 2",
+      },
+      {
+        label: "Buy green electricity",
+        data: [0, 0, 1.4],
+        borderWidth: 1,
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        stack: "Stack 2",
+      },
+      {
+        label: "Switch to electric car",
+        data: [0, 0, 1.4],
+        borderWidth: 1,
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        stack: "Stack 2",
+      },
+      {
+        label: "Plant based diet",
+        data: [0, 0, 0.4],
+        borderWidth: 1,
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        stack: "Stack 2",
+      },
+      {
+        label: "European flight",
+        data: [0, 0, 0.2],
+        borderWidth: 1,
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        stack: "Stack 2",
+      },
+      {
+        label: "Recycle",
+        data: [0, 0, 0.2],
+        borderWidth: 1,
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        stack: "Stack 2",
+      },
+      {
+        label: "Buy LEDs",
+        data: [0, 0, 0.03],
+        borderWidth: 1,
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        stack: "Stack 2",
+      },
+      {
+        label: "Reuse plastic bag 1000X",
+        data: [0, 0, 0.01],
+        borderWidth: 1,
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        stack: "Stack 2",
       },
     ],
   },
   options: {
-    indexAxis: "y",
+    //indexAxis: "y",
     plugins: {
       title: {
         display: true,
@@ -43,6 +107,22 @@ new Chart(ctx, {
       legend: {
         display: false,
       },
+    },
+    scales: {
+      xAxes: [
+        {
+          stacked: true,
+        },
+      ],
+      yAxes: [
+        {
+          stacked: true,
+          ticks: {
+            beginAtZero: true,
+          },
+          type: "linear",
+        },
+      ],
     },
   },
 });
